8d91bbdddb1dae4cedf7da9db1a60157d0108c97
Harshit Jain <raysofhope9@gmail.com>
2017-11-28 16:07:22 +0530

    fixed hist fetch

--- weather/historical_fetch.py
+++ weather/historical_fetch.py
@@ -1,16 +1,15 @@
 import json
 import requests
 import psycopg2
+import datetime
 
 conf_file = open("/home/ec2-user/conf.json","r")
 conf = json.loads(conf_file.read())
 conf_file.close()
 
+hist_data_url = "http://api.wunderground.com/api/b44e472e7b8b1828/history_201711{}/q/NY/New_York.json"
 rds_client = psycopg2.connect(database=conf["db"], user = conf["user"],\
             password = conf["password"], host = conf["host"], port = conf["port"])
-hist_data_url = "http://api.wunderground.com/api/b44e472e7b8b1828/history_201711{}/q/NY/New_York.json"
-
-last_dt = 9
 cur = rds_client.cursor()
 
 for i in range(10,28):
@@ -18,13 +17,13 @@ for i in range(10,28):
     payload = json.loads(resp.content)
     hourly_data = payload["history"]["observations"]
     for data in hourly_data:
-        dt = "2017-11-"+str(i)
+        dt = datetime.date(2017,11,i)
         hour = data["utcdate"]["hour"]
         temp = data["tempm"]
-        humi = data["hum"]
+        humi = None if data["hum"]=="N/A" else data["hum"]
         wspd = data["wspdm"]
         query = "insert into public.data_service_hourlyweatherdata \
         (hour,date,temp_celsius,humidity_percent,windspeed_kmph) values \
-        ({},{},{},{},{}) on conflict do nothing".format(hour,dt,temp,humi,wspd)
-        cur.execute(query)
+        (%s,%s,%s,%s,%s) on conflict do nothing"
+        cur.execute(query,(int(hour),dt,temp,humi,wspd))
     rds_client.commit()
